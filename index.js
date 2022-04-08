bdbf87dbe337e0355194565a801ca4aaf9748ad4
Andy Cheng <andy@95539500.com>
2022-04-08 16:53:46 +0100

    generated to 500

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+for(let i=0; i<300; i++) {
+    let filename = i.toString(16).padStart(64, "0") + ".json";
+    let content = `{
+        "description": "GameItems - ${i}",
+        "external_url": "https://forum.openzeppelin.com",
+        "image": "https://openmoji.org/data/color/svg/1FA99.svg",
+        "name": "Gold"
+      }`
+    fs.writeFile('data/' + filename, content, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
+        //file written successfully
+      })
+}
+
